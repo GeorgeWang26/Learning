4d8ab2af0a109c297a92e8a4286e1b4ca9e41eff
ZhengWang26 <wzheng2013@gmail.com>
2022-06-06 14:34:47 -0400

    docker compatible in arm64

--- ros2_autocompile/aes_encrypt.py
+++ ros2_autocompile/aes_encrypt.py
@@ -5,16 +5,28 @@ from Crypto.Cipher import AES
 from Crypto.Protocol.KDF import scrypt
 from getmac import get_mac_address as gma
 import subprocess
+from os import path
 
 
 data = "WHJLQJH:OIE@*(#_!*$!@U$B!@Y$_(*!@&JHJ#K!B#I!@*)(#"
 file_location = "/data/encrypted"
 
 def get_password():
-    s = subprocess.Popen("sudo dmidecode -t system | grep UUID", shell=True, stdout=subprocess.PIPE).stdout.read()
-    s = s.decode()[7:-1]
-    pwd = gma() + s
-    print("password:", pwd)
+    cpu_archetecture = subprocess.Popen("uname -m", shell=True, stdout=subprocess.PIPE).stdout.read().decode()[:-1]
+    if cpu_archetecture == "aarch64":
+        device_tree = "/proc/device-tree/"
+        if path.exists("/Indro"):
+            device_tree = "/Indro/"
+        serial = subprocess.Popen("cat " + device_tree + "serial-number", shell=True, stdout=subprocess.PIPE).stdout.read().decode()[:-1]
+        uuid = subprocess.Popen("cat " + device_tree + "chosen/uuid", shell=True, stdout=subprocess.PIPE).stdout.read().decode()[:-1]
+    elif cpu_archetecture == "x86_64":
+        serial = subprocess.Popen("sudo dmidecode -t system | grep Serial", shell=True, stdout=subprocess.PIPE).stdout.read().decode()[16:-1]
+        uuid = subprocess.Popen("sudo dmidecode -t system | grep UUID", shell=True, stdout=subprocess.PIPE).stdout.read().decode()[7:-1]
+    else:
+        print("===================== ERROR: unrecognized cpu archetecture =====================")
+        quit()
+    pwd = gma() + serial + uuid
+    # print("password:", pwd)
     return pwd
 
 def encrypt():
